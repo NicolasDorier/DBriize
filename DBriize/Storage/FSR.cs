fa7bc4d2874061bb569267ddf9f177470256b7e3
nicolas.dorier <nicolas.dorier@gmail.com>
2019-04-26 12:01:30 +0900

    Fix DBriize on old db

--- DBriize/Storage/FSR.cs
+++ DBriize/Storage/FSR.cs
@@ -451,16 +451,9 @@ namespace DBriize.Storage
         }
 #else
 
-        [System.Runtime.InteropServices.DllImport("kernel32.dll", ExactSpelling = true, SetLastError = true)]
-        private static extern bool FlushFileBuffers(IntPtr hFile);
-
         public static void NET_Flush(FileStream mfs)
         {
-            mfs.Flush();
-            IntPtr handle = mfs.SafeFileHandle.DangerousGetHandle();
-
-            if (!FlushFileBuffers(handle))
-                throw new System.ComponentModel.Win32Exception();
+            mfs.Flush(true);
         }
 #endif
         #endregion
